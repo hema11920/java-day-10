92c8a72ac46fde64b6f9358984143373c50a5c52
hema11920 <kasireddygarihemasree@gmail.com>
2024-12-11 13:42:55 +0530

    Add files via upload

---
+++ staticmethodwithprotectedAM.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class staticmethodwithprotectedAM{
+    public static void s(){
+        System.out.println("hello students");
+    }
+    public static void main(String args[]){
+        s();
+    }
+}
